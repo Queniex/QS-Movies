6e31674106a2208fff3d3d68625c9328a04c5a7e
Queniex <Queniesalbiyah@gmail.com>
2023-09-02 21:39:46 +0700

    add logo component

---
+++ src/components/logo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-white font-bold text-3xl flex justify-start h-14 items-center">
+        <h1>Q.S <span class="text-[#4194D8]">MOVIES</span></h1>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'Logo',
+    props: {
+    }
+}
+</script>
+  
+<style scoped>
+/* * {
+    border: 1px solid red;
+} */
+</style>
+  
