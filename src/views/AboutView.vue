322bbbe7f0c9c61fb3631b4a282b00713e2abc86
Queniex <Queniesalbiyah@gmail.com>
2023-09-02 12:36:06 +0700

    Setup Project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <div class="bg-black h-20 w-20">
+      <h2 class="text-white">Hello</h2>
+    </div>
+    <h1>This is an about page</h1>
+  </div>
+</template>
