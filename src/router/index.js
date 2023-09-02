c487c95ac234583342057708dc6393f2e04c44dc
Queniex <Queniesalbiyah@gmail.com>
2023-09-02 21:39:27 +0700

    Setting up index page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MovieGenreView from '../views/MovieGenreView.vue'
+import MovieSearchView from '../views/MovieSearchView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/genre',
+    name: 'movie-genre',
+    component: MovieGenreView
+  },
+  {
+    path: '/search',
+    name: 'movie-search',
+    component: MovieSearchView
   }
 ]
 
