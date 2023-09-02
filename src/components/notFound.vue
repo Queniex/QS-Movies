a3c43f268cf2cf4ae101b06d172e2da69fc8f47b
Queniex <Queniesalbiyah@gmail.com>
2023-09-02 21:40:00 +0700

    add not found component

---
+++ src/components/notFound.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="not-found">
+        <div class="flex justify-center items-center mt-10">
+            <div class="w-1/4 h-1/4 text-center">
+                <h1 class="text-white font-semibold mb-5">"Aw, there's <span class="text-blue-300">no keyword</span>
+                    found..."</h1>
+                <img src="https://i.postimg.cc/fRwJBVcP/fff32b7ec8fd731c40f575d85b3ca0a9-removebg-preview.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'NotFound',
+    props: {
+
+    }
+}
+</script>
+  
+<style scoped>
+/* * {
+    border: 1px solid red;
+} */
+</style>
+  
