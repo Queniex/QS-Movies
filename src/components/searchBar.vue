08920d5c6f645fc32986324efa2f14217bd768ea
Queniex <Queniesalbiyah@gmail.com>
2023-09-03 20:40:43 +0700

    Sent data from componen to parent

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <form>
+        <form @submit.prevent="sendSearchData">
             <label for="default-search"
                 class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative">
@@ -13,22 +13,30 @@
                 </div>
                 <input type="search" id="default-search"
                     class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#D9D9D9] dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search Movie, Film, Drama..." required>
+                    placeholder="Search Movie, Film, Drama..." required v-model="search">
                 <button type="submit"
-                    class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Search
+                </button>
             </div>
         </form>
     </div>
 </template>
-  
+
 <script>
 export default {
     name: 'SearchBar',
-    props: {
-
+    data() {
+        return {
+            search: ''
+        }
+    },
+    methods: {
+        sendSearchData() {
+            this.$emit("sbEvent", this.search);
+        }
     }
 }
 </script>
-  
+
 <style scoped></style>
-  
